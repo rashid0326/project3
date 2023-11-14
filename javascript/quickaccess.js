9e7721e0d0d67fd666811d564d6a097e3f9ab817
rashid0326 <143540850+rashid0326@users.noreply.github.com>
2023-11-14 09:46:35 -0600

    Update quickaccess.js

--- javascript/quickaccess.js
+++ javascript/quickaccess.js
@@ -1,21 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
   var quickLinksContainer = document.getElementById("quick-links");
 
-  // Create YouTube link
+  // Create YouTube link and logo
   var youtubeLink = document.createElement("li");
   var youtubeAnchor = document.createElement("a");
   youtubeAnchor.href = "https://www.youtube.com";
   youtubeAnchor.textContent = "YouTube";
   youtubeLink.appendChild(youtubeAnchor);
 
-  // Create Google link
+  var youtubeLogo = document.createElement("li");
+  var youtubeLogoImg = document.createElement("img");
+  youtubeLogoImg.src = "Img/images/youtube.jpg"; // Replace with the actual path to your YouTube logo image
+  youtubeLogoImg.alt = "YouTube Logo";
+  youtubeLogo.appendChild(youtubeLogoImg);
+
+  // Create Google link and logo
   var googleLink = document.createElement("li");
   var googleAnchor = document.createElement("a");
   googleAnchor.href = "https://www.google.com";
   googleAnchor.textContent = "Google";
   googleLink.appendChild(googleAnchor);
 
-  // Append links to the Quick Access section
+  var googleLogo = document.createElement("li");
+  var googleLogoImg = document.createElement("img");
+  googleLogoImg.src = "Img/images/google.jpg"; // Replace with the actual path to your Google logo image
+  googleLogoImg.alt = "Google Logo";
+  googleLogo.appendChild(googleLogoImg);
+
+  // Append links and logos to the Quick Access section
   quickLinksContainer.appendChild(youtubeLink);
+  quickLinksContainer.appendChild(youtubeLogo);
   quickLinksContainer.appendChild(googleLink);
+  quickLinksContainer.appendChild(googleLogo);
 });
